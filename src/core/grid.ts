e7e4b45a7dbbd19ca1765924cdb63eb217d9ec93
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2021-01-25 09:18:06 +0000

    Add implementation

--- src/core/grid.ts
+++ src/core/grid.ts
@@ -36,19 +36,35 @@ export const createGrid = (columns: Column[]): Grid => {
 
 export const addColumn = ({ columns, ...rest }: Grid, input: Column): Grid => {
   checkColumn(input);
-  
+
   const remainingWidth = MAX_WIDTH - input.width;
   if (remainingWidth <= 0) {
     throw new Error("Cannot provide space for new column");
   }
-  
+
   const existingColumns = columns.map(column => ({
     ...column,
     width: remainingWidth / columns.length
   }));
-  
+
   return {
     ...rest,
     columns: [...existingColumns, input]
-  }; 
+  };
+};
+
+export const popColumn = ({ columns, ...rest }: Grid): Grid => {
+  if (columns.length === 1) {
+    throw new Error("Grid must have at least 1 column");
+  }
+
+  const remainingColumns = columns.slice(0, columns.length - 1);
+
+  return {
+    ...rest,
+    columns: remainingColumns.map(column => ({
+      ...column,
+      width: MAX_WIDTH / remainingColumns.length
+    }))
+  };
 };
