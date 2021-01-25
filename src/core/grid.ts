d1d1368ecf69c6b1e82186a89bfa2c5f3007664f
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2021-01-25 08:46:08 +0000

    Add implementation

--- src/core/grid.ts
+++ src/core/grid.ts
@@ -7,6 +7,29 @@ export interface Grid {
   columns: Column[];
 }
 
+export const MAX_WIDTH = 12;
+
+const checkColumn = (column: Column) => {
+  if (column.width < 1) {
+    throw new Error("Column must be at least 1 wide");
+  }
+
+  if (column.width > MAX_WIDTH) {
+    throw new Error(`Column cannot be more than ${MAX_WIDTH} wide`);
+  }
+};
+
 export const createGrid = (columns: Column[]): Grid => {
-  return { columns: [] };
-}
+  if (columns.length === 0) {
+    throw new Error("Grid must have at least 1 column");
+  }
+
+  columns.forEach(checkColumn);
+
+  const totalWidths = columns.reduce((acc, column) => acc + column.width, 0);
+  if (totalWidths !== MAX_WIDTH) {
+    throw new Error(`Column widths must add up to ${MAX_WIDTH}`);
+  }
+
+  return { columns };
+};
