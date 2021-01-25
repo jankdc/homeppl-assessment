c65d34a96fb8eebee4396f95b0da70d8b6eb7762
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2021-01-25 10:28:21 +0000

    Add implementation

---
+++ src/components/grid.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+
+import type { Column as ColumnData, Grid as GridData } from "../core/grid";
+
+export interface GridProps {
+  data: GridData;
+}
+
+export interface ColumnProps {
+  data: ColumnData;
+}
+
+export const Column = ({ data }: ColumnProps) => (
+  <div className={`column width-${data.width}`}>
+    {data.content ?? ""}
+  </div>
+);
+
+export const Grid = ({ data }: GridProps) => (
+  <div className="grid">
+    { data.columns.map((column, index) => <Column key={`column-${index}`} data={column} />)}
+  </div>
+);
